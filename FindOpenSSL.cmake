3305d0c58a982e8ed25a77228b27f2d99760e745
Daniel Witt <dan.witt@gmail.com>
2023-08-15 13:56:24 -0500

    experimental change

--- FindOpenSSL.cmake
+++ FindOpenSSL.cmake
@@ -7,11 +7,12 @@ if(OPENSSL_DIR)
 endif()
 
 # Check if OpenSSLConfig.cmake is present in the provided path
-find_package(OpenSSL QUIET CONFIG)
+find_package(OpenSSL CONFIG)
 
 if(NOT OpenSSL_FOUND)
     message(FATAL_ERROR "Could not find built version of OpenSSL. Please set OPENSSL_DIR or provide OpenSSLConfig.cmake.")
 else()
+    set( OPENSSL_DIR ${CMAKE_CURRENT_LIST_DIR} PARENT_SCOPE)
     # If found, the imported targets OpenSSL::SSL and OpenSSL::Crypto would be available for use.
 endif()
 
