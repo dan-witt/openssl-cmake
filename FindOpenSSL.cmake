cb2a20e4d6f8270dfe45f1dafea53f3f89ef5248
Daniel Witt <dan.witt@gmail.com>
2023-08-15 13:58:27 -0500

    typo

--- FindOpenSSL.cmake
+++ FindOpenSSL.cmake
@@ -12,7 +12,7 @@ find_package(OpenSSL CONFIG)
 if(NOT OpenSSL_FOUND)
     message(FATAL_ERROR "Could not find built version of OpenSSL. Please set OPENSSL_DIR or provide OpenSSLConfig.cmake.")
 else()
-    set( OPENSSL_DIR ${CMAKE_CURRENT_LIST_DIR} PARENT_SCOPE)
+	set( OpenSSL_DIR ${CMAKE_CURRENT_LIST_DIR} PARENT_SCOPE)
     # If found, the imported targets OpenSSL::SSL and OpenSSL::Crypto would be available for use.
 endif()
 
