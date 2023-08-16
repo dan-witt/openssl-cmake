d9172af2781b55d5d76e10be393577bf02f9e946
Daniel Witt <dan.witt@gmail.com>
2023-08-16 11:04:05 -0500

    bump android version to 23 for stderr and friends fix OpenSSLConfig.cmake so it should hopefully work now

--- OpenSSLConfig.cmake
+++ OpenSSLConfig.cmake
@@ -18,16 +18,23 @@ endif()
 
 # Set the include and library paths.
 set(OPENSSL_INCLUDE_DIRS "${CMAKE_CURRENT_LIST_DIR}/include")
-set(OPENSSL_LIBRARIES "${CMAKE_CURRENT_LIST_DIR}/lib/${OPENSSL_ARCH}/libssl.a")
-set(OPENSSL_CRYPTO_LIBRARIES "${CMAKE_CURRENT_LIST_DIR}/lib/${OPENSSL_ARCH}/libcrypto.a")
+set(OPENSSL_SSL_LIBRARY "${CMAKE_CURRENT_LIST_DIR}/lib/${OPENSSL_ARCH}/libssl.a")
+set(OPENSSL_CRYPTO_LIBRARY "${CMAKE_CURRENT_LIST_DIR}/lib/${OPENSSL_ARCH}/libcrypto.a")
 
 # Define an IMPORTED library target.
 add_library(OpenSSL::SSL  STATIC IMPORTED)
-set_target_properties(OpenSSL::SSL  PROPERTIES IMPORTED_LOCATION "${OPENSSL_LIBRARIES}")
+set_target_properties(OpenSSL::SSL  PROPERTIES IMPORTED_LOCATION "${OPENSSL_SSL_LIBRARY}")
 target_include_directories(OpenSSL::SSL  INTERFACE ${OPENSSL_INCLUDE_DIRS})
+if(DEFINED ANDROID_ABI)
+    message(STATUS "linking against android libraries")
+    set_property(TARGET OpenSSL::SSL APPEND_STRING PROPERTY INTERFACE_LINK_LIBRARIES log android c)
+endif()
 
 add_library(OpenSSL::Crypto STATIC IMPORTED)
-set_target_properties(OpenSSL::Crypto PROPERTIES IMPORTED_LOCATION "${OPENSSL_CRYPTO_LIBRARIES}")
+set_target_properties(OpenSSL::Crypto PROPERTIES IMPORTED_LOCATION "${OPENSSL_CRYPTO_LIBRARY}")
 target_include_directories(OpenSSL::Crypto INTERFACE ${OPENSSL_INCLUDE_DIRS})
+if(DEFINED ANDROID_ABI)
+    set_property(TARGET OpenSSL::Crypto APPEND_STRING PROPERTY INTERFACE_LINK_LIBRARIES log android c)
+endif()
 
 set(OpenSSL_FOUND TRUE)
